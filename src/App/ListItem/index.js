07795d47daf7d9aada08d9e4fe7f491a7871ea46
amymc <amy.mccarthy@hmhco.com>
2018-05-09 22:54:20 +0100

    wip: parse published at date

--- src/App/ListItem/index.js
+++ src/App/ListItem/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { css } from 'react-emotion';
+import moment from 'moment';
 
 const listItem = css`
   display: flex;
@@ -10,6 +11,18 @@ const image = css`
   width: 100px;
   height: 63px;
 `;
+
+const parseTimestamp = timestamp => {
+  const timePublished = moment(timestamp, 'YYYY-MM-DD HH:mm');
+  // mocking a time close to the publication dates
+  // so sorting by submitted time will be more meaningful
+  const mockCurrentTime = moment('2013-11-09 13:20:00', 'YYYY-MM-DD HH:mm');
+  const diff = Math.abs(timePublished.diff(mockCurrentTime, 'minutes'));
+
+  const parsedResult =
+    diff > 60 ? `${Math.round(diff / 60)} hours` : `${diff} minutes`;
+  return parsedResult;
+};
 
 class ListItem extends Component {
   render() {
@@ -22,7 +35,7 @@ class ListItem extends Component {
           {item.first_name} {item.last_name}
         </span>
         <span>{item.words}</span>
-        <span>{item.publish_at}</span>
+        <span>{parseTimestamp(item.publish_at)} ago</span>
       </li>
     );
   }
