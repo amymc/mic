dd4f2e16bfa084786c06fffb0111c345b2dc8dd8
amymc <amy.mccarthy@hmhco.com>
2018-05-09 23:07:43 +0100

    wip: header styles

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { css } from 'react-emotion';
+
+const header = css`
+  position: fixed;
+  background-color: #0be6af;
+  padding: 1rem;
+  width: 100%;
+`;
 
 class Header extends Component {
   render() {
     return (
-      <header className="App-header">
+      <header className={header}>
         <span>Unpublished articles</span>
         <span>Author</span>
         <span>Words</span>
