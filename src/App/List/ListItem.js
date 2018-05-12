cce534727a509028c152fe4e82b267d92833aa36
amymc <amy.mccarthy@hmhco.com>
2018-05-12 18:36:42 +0100

    refactor: move parseTimestamp to utils

--- src/App/List/ListItem.js
+++ src/App/List/ListItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { css } from 'react-emotion';
-import moment from 'moment';
+import { parseTimestamp } from '../../utils';
 
 const base = css`
   margin-bottom: 0.5rem;
@@ -39,23 +39,6 @@ const image = css`
 const title = css``;
 // width: 60%;
 //   flex-grow: 1;
-
-const maybePluralise = (count, noun, suffix = 's') =>
-  `${count} ${noun}${count !== 1 ? suffix : ''}`;
-
-const parseTimestamp = timestamp => {
-  const timePublished = moment(timestamp, 'YYYY-MM-DD HH:mm');
-  // mocking a time close to the publication dates
-  // so sorting by submitted time will be more meaningful
-  const mockCurrentTime = moment('2013-11-09 13:20:00', 'YYYY-MM-DD HH:mm');
-  const diff = Math.abs(timePublished.diff(mockCurrentTime, 'minutes'));
-
-  const parsedResult =
-    diff > 60
-      ? `${maybePluralise(Math.round(diff / 60), 'hour')}`
-      : `${maybePluralise(diff, 'minute')}`;
-  return parsedResult;
-};
 
 class ListItem extends Component {
   render() {
